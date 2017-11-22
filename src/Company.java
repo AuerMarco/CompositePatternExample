d6aee74373d36f99ffd7b15b6a1358156427be3c
Marco Auer <marcoa91@gmx.at>
2017-11-23 00:01:51 +0100

    Add a Powerpoint presentation

--- src/Company.java
+++ src/Company.java
@@ -6,18 +6,17 @@
 public class Company {
     public static void main(String[] args) {
         Manager m = new Manager("Manager", 999);
-        Employee e = new Employee("Slave1", 99);
+        Employee e = new Employee("Employee 1", 99);
         m.add(e);
         Manager m2 = new Manager("Top Manager", 999);
-        Employee e2 = new Employee("Slave2", 99);
-        Employee e3 = new Employee("Slave3", 99);
+        Employee e2 = new Employee("Employee 2", 99);
+        Employee e3 = new Employee("Employee 3", 99);
         m2.add(m);
         m2.add(e2);
         m2.add(e3);
-        m2.getAllSubordinates();
-        System.out.println("-----");
+        System.out.println("-----Top Manager hierarchy-----");
         m2.print();
-        System.out.println("-----Big Boss hirarchy-----");
+        System.out.println("-----Big Boss hierarchy-----");
         Manager bigBoss = new Manager("Big Boss", 9999);
         bigBoss.add(m2);
         bigBoss.print();
@@ -25,7 +24,7 @@ public class Company {
 //        for (EmployeeGeneral emp : m2.getSubordinates()) {
 //            System.out.println(emp.toString());
 //        }
-        System.out.println("-----Big Boss hirarchy after remove-----");
+        System.out.println("-----Big Boss hierarchy after remove-----");
         m2.remove(e3);
         bigBoss.print();
     }
